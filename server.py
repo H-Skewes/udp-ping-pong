04eebfbaeea6aac3b0dfdf36f2ca8091826120c8
Henry Skewes <info@henryskewes.com>
2026-04-20 22:41:34 -0500

    fixed server

--- server.py
+++ server.py
@@ -11,14 +11,14 @@ def server():
     # confirming server up
     print(f"[server] : ready to accept data...")
     # loop for socket to listen continuosly and respond at the ip and port
-    while True:
+    count = 0
+    while count < 10:
         data, client_address = server_socket.recvfrom(1024)
-
         if data == b'PING':
-            print(f"[client] : {data.decode()}")
-
+            count += 1
             # Introduces the random packet drop
             if random.random() < 0.70:
+                print(f"[client] : {data.decode()}")
                 server_socket.sendto(b'PONG', client_address)
             else:
                 print(f"[server] : packet dropped")
